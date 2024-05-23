a715da99b48ab62e2dc452f9edcd87a05aad2b46
lukasz.daniel <lukasz.daniel@gmail.com>
2024-05-23 15:17:43 +0200

    Changes in files scan and source reverted.

--- src/main/scan.cpp
+++ src/main/scan.cpp
@@ -322,6 +322,20 @@ static int scanchar(bool inQuote, LocalData *d)
     return next;
 }
 
+/* utility to close connections after interrupts */
+static void scan_cleanup(void *data)
+{
+    LocalData *ld = (LocalData *) data;
+    if(ld->con && !ld->ttyflag && !ld->wasopen) {
+	ld->con->close(ld->con);
+	ld->con = NULL;
+    }
+    if(ld->quoteset && ld->quoteset[0]) {
+	free((void *)ld->quoteset);
+	ld->quoteset = NULL;
+    }
+}
+
 #include "RBufferUtils.h"
 
 /*XX  Can we pass this routine an R_StringBuffer? appears so.
@@ -923,7 +937,11 @@ attribute_hidden SEXP do_scan(SEXP call, SEXP op, SEXP args, SEXP rho)
 
     /* set up a context which will close the connection if there is
        an error or user interrupt */
-    try {
+    RCNTXT cntxt(CTXT_CCODE, R_GlobalContext->call, R_BaseEnv,
+		 R_BaseEnv, R_NilValue, R_NilValue);
+    cntxt.cend = &scan_cleanup;
+    cntxt.cenddata = &data;
+
     if (isString(quotes)) {
 	const char *sc = translateChar(STRING_ELT(quotes, 0));
 	if (strlen(sc)) data.quoteset = Rstrdup(sc);
@@ -998,17 +1016,8 @@ attribute_hidden SEXP do_scan(SEXP call, SEXP op, SEXP args, SEXP rho)
     default:
 	error(_("invalid '%s' argument"), "what");
     }
-    } catch (...) {
-        if (data.con && !data.ttyflag && !data.wasopen) {
-            data.con->close(data.con);
-            data.con = NULL;
-        }
-        if (data.quoteset && data.quoteset[0]) {
-            free((void *)data.quoteset);
-            data.quoteset = NULL;
-        }
-        throw;
-    }
+
+    endcontext(&cntxt);
 
     /* we might have a character that was unscanchar-ed.
        So pushback if possible */
--- src/main/source.cpp
+++ src/main/source.cpp
@@ -194,6 +194,16 @@ typedef struct parse_info {
     bool old_utf8;
 }  parse_cleanup_info;
 
+static void parse_cleanup(void *data)
+{
+    parse_cleanup_info *pci = (parse_cleanup_info *)data;
+    Rconnection con = pci->con;
+    if(con && con->isopen)
+	con->close(con);
+    known_to_be_latin1 = pci->old_latin1;
+    known_to_be_utf8 = pci->old_utf8;
+}
+
 /* "do_parse" - the user interface input/output to files.
 
  The internal R_Parse.. functions are defined in ./gram.y (-> gram.c)
@@ -234,7 +244,11 @@ attribute_hidden SEXP do_parse(SEXP call, SEXP op, SEXP args, SEXP env)
     GCRoot<> s(R_NilValue);
     /* set up context to recover known_to_be_* and to close connection on
        error if opened by do_parse */
-    try {
+    RCNTXT cntxt(CTXT_CCODE, R_NilValue, R_BaseEnv, R_BaseEnv,
+		 R_NilValue, R_NilValue);
+    cntxt.cend = &parse_cleanup;
+    cntxt.cenddata = &pci;
+
     known_to_be_latin1 = known_to_be_utf8 = FALSE;
     bool allKnown = TRUE;
     /* allow 'encoding' to override declaration on 'text'. */
@@ -305,15 +319,8 @@ attribute_hidden SEXP do_parse(SEXP call, SEXP op, SEXP args, SEXP env)
 
     known_to_be_latin1 = pci.old_latin1;
     known_to_be_utf8 = pci.old_utf8;
-	} catch (...)
-	{
-        Rconnection con = pci.con;
-        if (con && con->isopen)
-            con->close(con);
-        known_to_be_latin1 = pci.old_latin1;
-        known_to_be_utf8 = pci.old_utf8;
-        throw;
-	}
+
+    endcontext(&cntxt);
 
     return s;
 }
