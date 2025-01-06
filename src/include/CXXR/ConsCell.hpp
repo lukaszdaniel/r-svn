327daa520995f2c52946f7677d1ff8d8731f3f57
Łukasz Daniel <lukasz.daniel@gmail.com>
2025-01-06 19:10:19 +0100

    Bug fix in ConsCell.hpp.

--- src/include/CXXR/ConsCell.hpp
+++ src/include/CXXR/ConsCell.hpp
@@ -179,7 +179,7 @@ namespace CXXR
 
             void advance()
             {
-                if (m_cc && (m_cc->tail() != R_NilValue))
+                if (m_cc && (static_cast<const RObject *>(m_cc->tail()) != R_NilValue))
                 {
                     m_cc = static_cast<const ValueType *>(m_cc->tail());
                 }
